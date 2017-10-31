88b0649d3d8fc5a5592e3553f2d4bc43fc67c3d5
jamie <jmleary@willamette.edu>
2017-10-31 15:58:41 -0700

    Add Persistence, Start/Stop scripts, editing, etc.

--- src/Main.hs
+++ src/Main.hs
@@ -11,7 +11,9 @@ import Control.Applicative
 import Control.Monad
 
 import Project
+import TempFile
 
+-- Verb type represents the commands that can be passed in 
 data Verb
   = List
   | Start
@@ -26,24 +28,27 @@ data Verb
   | Finish
   deriving (Eq, Ord, Bounded, Enum, Show, Read)
 
-getPmDir :: IO FilePath
-getPmDir = do
+data EditVerb
+  = EditName
+  | EditDesc
+  | EditStart
+  | EditStop
+  | EditTodos
+  deriving (Eq, Ord, Bounded, Enum, Show, Read)
+
+-- The PM File is an XML document that contains information about current
+-- projects
+getPmFile :: IO FilePath
+getPmFile = do
   env <- M.fromList <$> getEnvironment
   home <- getHomeDirectory
-  return $ M.findWithDefault (home ++ "/.pm/") "PM_HOME" env
+  return $ M.findWithDefault (home ++ "/.pm.xml") "PM_PROJECT_FILE" env
 
-getPmCur :: IO FilePath
-getPmCur = do
-  env <- M.fromList <$> getEnvironment
-  home <- getHomeDirectory
-  return $ M.findWithDefault (home ++ "/.pm_current") "PM_CURRENT" env
-
+-- Print out usage and exit
 usage :: IO a
 usage = putStrLn "usage: pm <verb> [project] (more help soon)" >> exitSuccess
 
-capitalize "" = ""
-capitalize (c:cs) = toUpper c : cs
-
+-- Look at the arguments and extract the Verb that is passed in
 getVerb :: IO Verb
 getVerb = do
   args <- getArgs
@@ -54,23 +59,86 @@ getVerb = do
                 then return $ read vs
                 else usage
 
+capitalize "" = ""
+capitalize (c:cs) = toUpper c : cs
+
+-- get all projects and display them
+listProjects pf = getProjects pf >>= printProjects
+
 main = do
-  pmdir <- getPmDir
-  pmcur <- getPmCur
+  pmfile <- getPmFile
   verb  <- getVerb
-  print pmdir
-  print pmcur
+  print pmfile
   print verb
   case verb of
-    List -> listTasks
+    List -> listProjects pmfile
+    Edit -> editProject pmfile
+    Create -> createProject pmfile
+    Stop -> stopCurrentProject pmfile
+    Start -> startNewProject pmfile
     _    -> putStrLn "Not Implemented Yet"
 
-getTasks = return [Project "proj1" "blurb" "desc" "echo hello" "echo goodbye" []]
+stopCurrentProject pmfile = do
+  p@(Projects cid ps) <- getProjects pmfile
+  case getCurrentProject p of
+    Nothing -> putStrLn "No active project"
+    Just cp@(Project _ _ _ _ _ stop _) ->
+      runString stop >> writeProjects pmfile (Projects Nothing ps)
+
+startNewProject pmfile = do
+  (v:pn:_) <- getArgs
+  a <- getProject pmfile pn
+  case a of
+    Nothing -> putStrLn $ "Error: project " ++ pn ++ " does not exist."
+    Just proj -> do
+      (Projects _ ps) <- getProjects pmfile
+      stopCurrentProject pmfile
+      runString $ start proj
+      writeProjects pmfile $ Projects (Just $ getId proj) ps
+
+editProject fp = do
+  ev <- getEditVerb
+  (v1:v2:p:_) <- getArgs
+  project <- getProject fp p
+  case project of
+    Nothing -> putStrLn "unknown project, see pm list"
+    Just p@(Project id name blurb desc start stop todos)
+      -> case ev of
+          EditDesc -> do
+            (newblurb:newdesc) <- lines <$> editString (blurb ++ "\n" ++ desc)
+            let newproj = Project id name newblurb (unlines newdesc) start stop todos
+            updateProject fp id newproj
+          EditName -> do
+            newname <- editString name
+            let newproj = Project id newname blurb desc start stop todos
+            updateProject fp id newproj
+          EditStart -> do
+            newstart <- editString start
+            let newproj = Project id name blurb desc newstart stop todos
+            updateProject fp id newproj
+          EditStop -> do
+            newstop <- editString stop
+            let newproj = Project id name blurb desc start newstop todos
+            updateProject fp id newproj
+          EditTodos -> do
+            putStrLn "not implemented yet lol that ones a little tricky"
+            putStrLn "sorry"
 
 
-printTask task = putStrLn "Here's a task!"
+getEditVerb :: IO EditVerb
+getEditVerb = do
+  args <- getArgs
+  case args of
+    (v1:v2:_) -> do
+                  let vs = "Edit" ++ capitalize v2
+                  if elem vs $ show <$> ([minBound..maxBound] :: [EditVerb])
+                    then return $ read vs
+                    else usage
+    _ -> putStrLn "No Verb" >> usage
 
-listTasks = do
-  tasks <- getTasks
-  mapM_ print tasks
-  mapM_ printTask tasks
+createProject pmfile = do
+  projects <- getProjects pmfile
+  (v1:n:b) <- getArgs
+  newid <- getNextId pmfile
+  let newproj = Project newid n (unwords b) "description goes here" "echo hello" "echo goodbye" []
+  writeProjects pmfile (addProject projects newproj)
--- src/Project.hs
+++ src/Project.hs
@@ -1,14 +1,152 @@
 module Project where
 
+import Control.Monad
+import Text.ParserCombinators.Poly.Parser
+import Text.XML.HaXml.TypeMapping
+import Text.XML.HaXml.XmlContent.Parser
+import Text.XML.HaXml.XmlContent
+import Text.XML.HaXml.Util
+import Text.XML.HaXml hiding (element)
+import Data.Maybe
 
+data ProjectList = Projects (Maybe Int) [Project] deriving Show
+
+instance HTypeable ProjectList where
+  toHType (Projects current projects) = Defined "ProjectList" []
+    [Constr "Projects" [] [toHType current, toHType projects]]
+
+instance XmlContent ProjectList where
+
+  parseContents = do
+      let parseC e = return $ read <$> attrToText "current" (attrs e)
+      e <- element ["projects"]
+      interior e (Projects <$> parseC e <*> many parseContents)
+
+  toContents p@(Projects c ps) =
+    [mkAttrElemC "projects" currentattribute (toContents ps)]
+    where
+      currentattribute = case c of
+        Nothing -> []
+        Just c -> [mkAttr "current" (show c)]
 
 data Project = Project {
-                  name:: String,
-                  blurb:: String,
-                  desc:: String,
-                  start:: String,
-                  stop::String,
-                  todos:: [Todo]
+                  getId :: Int,
+                  name :: String,
+                  blurb :: String,
+                  desc :: String,
+                  start :: String,
+                  stop :: String,
+                  todos :: [Todo]
                   } deriving (Show)
 
+instance HTypeable Project where
+  toHType (Project id n b d a z ts) = Defined "Project" []
+    [Constr "Project" [] [toHType id, toHType n, toHType b, toHType d,
+      toHType a, toHType z, toHType ts]]
+
+instance XmlContent Project where
+  parseContents = do
+      let pId e = return . read . fromJust $ attrToText "id" $ attrs e
+      let pName = inElement "name" text
+      let pBlurb = inElement "blurb" text
+      let pDesc = inElement "desc" text
+      let pStart = inElement "start" text
+      let pStop = inElement "stop" text
+      let pTodos = inElement "todos" $ many (parseContents :: XMLParser Todo)
+      e <- element ["project"]
+      interior e (Project <$> pId e <*> pName <*> pBlurb 
+                          <*> pDesc <*> pStart <*> pStop <*> pTodos)
+
+  toContents (Project id n b d a z ts) =
+    [mkAttrElemC "project" [mkAttr "id" (show id)] [
+      mkElemC "name" (toText n),
+      mkElemC "blurb" (toText b),
+      mkElemC "desc" (toText d),
+      mkElemC "start" (toText a),
+      mkElemC "stop" (toText z),
+      mkElemC "todos" (if null ts then [] else toContents ts)]]
+
+
 data Todo = Todo Int String deriving Show
+
+instance HTypeable Todo where
+  toHType (Todo num task) = Defined "Todo" []
+    [Constr "Todo" [] [toHType num, toHType task]]
+
+instance XmlContent Todo where
+  parseContents = do
+      let pId e = return . read . fromJust $ attrToText "id" $ attrs e
+      e <- element ["todo"]
+      interior e (Todo <$> pId e <*> text)
+  toContents (Todo id task) =
+    [mkAttrElemC "todo" [mkAttr "id" (show id)] (toText task)]
+
+-- helper functions for HaXml
+mkAttrElemC :: String -> [Attribute] -> [Content ()] -> Content ()
+mkAttrElemC x as cs = CElem (Elem (N x) as cs) ()
+
+attrToText :: String -> [Attribute] -> Maybe String
+attrToText n [] = Nothing
+attrToText n as = foldl1 (<|>) attrText
+       where attrText = map (fromAttrToStr n) as 
+
+getProjects :: FilePath -> IO ProjectList
+getProjects = fReadXml
+
+writeProjects :: FilePath -> ProjectList -> IO ()
+writeProjects = fpsWriteXml
+
+printProjects :: ProjectList -> IO ()
+printProjects (Projects cur ps) = do
+  putStrLn "Here are your tasks:"
+  putStrLn "------"
+  mapM_ (printProject cur) ps
+  putStrLn "-----"
+
+-- display a single project as part of the list command (for info command, see
+-- printProjectInfo)
+printProject cur (Project id name blurb _ _ _ ts) = do
+  let selector = case cur of
+                    Nothing -> "    "
+                    Just c  -> if c == id then "  + " else "    "
+  let numts = show $ length ts
+  putStrLn $ selector ++ "[" ++ name ++ "] (" ++ numts ++ " todos)"
+  putStrLn $ "        " ++ blurb
+
+printProjectInfo (Project id name blurb desc _ _ ts) = do
+  putStrLn $ "    [" ++ name ++ "]"
+  putStrLn "--------"
+  putStrLn blurb
+  putStrLn desc
+  mapM_ showTodo ts
+
+showTodo (Todo id desc) = putStrLn $ "[ ] (" ++ show id ++ "): " ++ desc
+
+addProject :: ProjectList -> Project -> ProjectList
+addProject (Projects id ps) p = Projects id (p:ps)
+
+getProject pmfile n = do
+  (Projects _ ps) <- getProjects pmfile
+  let hasname p = name p == n
+  return $ (listToMaybe . filter hasname) ps
+
+updateProject:: FilePath -> Int -> Project -> IO ()
+updateProject pmfile id newproj = do
+  (Projects id' ps) <- getProjects pmfile
+  let updateProjects' ps = case ps of
+                            [] -> []
+                            (p@(Project pid _ _ _ _ _ _):rps) -> (if pid == id then newproj else p) : updateProjects' rps
+  writeProjects pmfile $ Projects id' $ updateProjects' ps
+
+getProjectById (Projects _ ps) n =
+  case filter ((== n) .getId) ps of
+    [] -> Nothing
+    [a] -> Just a
+    (a:as) -> Just a
+
+getCurrentProject p@(Projects id ps) = id >>= getProjectById p
+
+getNextId pmfile = do
+  let getNextId' n ps = if any ((==) n . getId) ps then getNextId' (n+1) ps else n
+  (Projects _ ps) <- getProjects pmfile
+  return $ getNextId' 0 ps
